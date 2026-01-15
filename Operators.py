fa534dbf3b9eed915291c68ed630daacda57745b
rajeshrf <rajesh.rf@gmail.com>
2026-01-15 10:50:29 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,15 @@
+#learn operators
+mylist=[1,2,3,4,5]
+
+""" #range operator
+for num in range(1,11,2):
+    print(num) 
+
+print ("new one ")
+for num1 in range(10):
+    print(num1)  """
+
+my=list(range(1,11))  #casting range to list
+print(my)
+for i in my:
+    print(i)
